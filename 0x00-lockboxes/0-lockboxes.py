83c6edea66d3d39492006b334d4aca57f8692391
KevinAndresG <3348@holbertonschool.com>
2022-04-21 18:49:51 -0500

    boxes locked

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+	You have n number of locked boxes in front of you.
+	Each box is numbered sequentially from 0 to n - 1
+	and each box may contain keys to the other boxes.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+            method that determines if all the boxes can be opened.
+    """
+    list = 1
+    dict = {0: "true"}
+    while list < len(boxes):
+        dict[list] = "false"
+        list += 1
+    count = 0
+    for i in boxes:
+        if dict[count] == "false":
+            return ("false")
+        for h in i:
+            if (h in dict.keys()):
+                dict[h] = "true"
+                for o in boxes[h]:
+                    dict[o] = "true"
+        count += 1
+    return ("true")
