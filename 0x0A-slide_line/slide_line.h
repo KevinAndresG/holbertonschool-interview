016bb633ce0669f4f5379920600c9ded8ba455ab
KevinAndresG <kevingar1414@gmail.com>
2022-06-30 00:18:07 -0500

    issues

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -7,5 +7,7 @@
 #include <stddef.h>
 
 int slide_line(int *line, size_t size, int direction);
+int *FillLeft(int *array, size_t size, size_t sizeOfNewArray);
+int *FillRight(int *array, size_t size, size_t sizeOfNewArray);
 
 #endif /* LISTS_H */
