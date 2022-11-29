6c2f5adb32999048ffb3be7a8f4e6e8937b2d25b
KevinAndresG <kevingar1414@gmail.com>
2022-11-29 17:40:35 -0500

    amount of rainwater retained.

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    return amount of rainwater retained.
+"""
+
+
+def rain(walls):
+    count = 0
+    count2 = 0
+    water = 0
+    water2 = 0
+    noWater = 0
+    while walls[0] == 0:
+        walls.pop(0)
+    while walls[len(walls) - 1] == 0:
+        walls.pop()
+    for i in walls:
+        count2 = count
+        if i != 0:
+            noWater = i
+        else:
+            while walls[count2] == 0:
+                count2 += 1
+            if noWater < walls[count2]:
+                water = noWater
+                n = 0
+                while n < water:
+                    water2 += 1
+                    n += 1
+            else:
+                water = walls[count2]
+                n = 0
+                while n < water:
+                    water2 += 1
+                    n += 1
+        count += 1
+    return water2
---
+++ 0x10-rain/README.md
@@ -0,0 +1,9 @@
+<p>Given a list of non-negative integers representing the heights of walls with unit width 1, as if viewing the cross-section of a relief map, calculate how many square units of water will be retained after it rains.</p>
+
+<ul>
+<li>Prototype: <code>def rain(walls)</code></li>
+<li><code>walls</code> is a list of non-negative integers.</li>
+<li>Return: Integer indicating total amount of rainwater retained.</li>
+<li>Assume that the ends of the list (before index 0 and after index walls[-1]) are <strong>not</strong> walls, meaning they will not retain water.</li>
+<li>If the list is empty return <code>0</code>.</li>
+</ul>
