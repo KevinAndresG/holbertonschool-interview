fcb9c9aa761d916c802687043360e28761f55179
KevinAndresG <kevingar1414@gmail.com>
2022-11-30 15:18:43 -0500

    fixing

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -5,38 +5,42 @@
 
 
 def rain(walls):
-    if not walls:
-        return 0
     count = 0
     count2 = 0
+    count3 = 0
     water = 0
     water2 = 0
     wall = 0
+    size = len(walls)
+    if not walls:
+        return 0
     while walls[0] == 0:
         walls.pop(0)
+        count3 += 1
+        if count3 == size -1:
+            return 0
     while walls[len(walls) - 1] == 0:
         walls.pop()
-    for i in walls:
-        count2 = count
-        if count != len(walls) - 1:
-            if i > 0:
-                wall = i
-            elif i == 0:
-                while walls[count2] == 0:
-                    count2 += 1
-                if wall <= walls[count2]:
-                    water = wall
-                    n = 0
-                    while n < water:
-                        water2 += 1
-                        n += 1
+    if walls:
+        for i in walls:
+            count2 = count
+            if count != len(walls) - 1:
+                if i > 0:
+                    wall = i
                 else:
-                    water = walls[count2]
-                    n = 0
-                    while n < water:
-                        water2 += 1
-                        n += 1
-            else:
-                return 0
-            count += 1
-    return water2
+                    while walls[count2] == 0:
+                        count2 += 1
+                    if wall <= walls[count2]:
+                        water = wall
+                        n = 0
+                        while n < water:
+                            water2 += 1
+                            n += 1
+                    else:
+                        water = walls[count2]
+                        n = 0
+                        while n < water:
+                            water2 += 1
+                            n += 1
+                count += 1
+        return water2
