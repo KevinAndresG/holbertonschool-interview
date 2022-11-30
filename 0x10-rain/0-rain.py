83ed569839fb0065e09c514383aa4858600b250f
KevinAndresG <kevingar1414@gmail.com>
2022-11-30 10:44:33 -0500

    fixing code

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -11,31 +11,32 @@ def rain(walls):
     count2 = 0
     water = 0
     water2 = 0
-    noWater = 0
+    wall = 0
     while walls[0] == 0:
         walls.pop(0)
     while walls[len(walls) - 1] == 0:
         walls.pop()
     for i in walls:
         count2 = count
-        if i != 0:
-            if i < 0:
-                return 0
-            noWater = i
-        else:
-            while walls[count2] == 0:
-                count2 += 1
-            if noWater < walls[count2]:
-                water = noWater
-                n = 0
-                while n < water:
-                    water2 += 1
-                    n += 1
+        if count != len(walls) - 1:
+            if i > 0:
+                wall = i
+            elif i == 0:
+                while walls[count2] == 0:
+                    count2 += 1
+                if wall <= walls[count2]:
+                    water = wall
+                    n = 0
+                    while n < water:
+                        water2 += 1
+                        n += 1
+                else:
+                    water = walls[count2]
+                    n = 0
+                    while n < water:
+                        water2 += 1
+                        n += 1
             else:
-                water = walls[count2]
-                n = 0
-                while n < water:
-                    water2 += 1
-                    n += 1
-        count += 1
+                return 0
+            count += 1 
     return water2
